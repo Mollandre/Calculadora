2576abf34e2c19503351a18e797835c947cecf6b
Mollandre <100574835+Mollandre@users.noreply.github.com>
2024-06-25 12:23:06 -0300

    Adicionar arquivos de projeto.

---
+++ primeiro projeto/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace primeiro_projeto
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNo2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNo1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cb1);
+            this.groupBox1.Controls.Add(this.lbResultado);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtNo2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNo1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(374, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cb1.Location = new System.Drawing.Point(238, 54);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(43, 21);
+            this.cb1.TabIndex = 10;
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultado.Location = new System.Drawing.Point(124, 227);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(22, 25);
+            this.lbResultado.TabIndex = 9;
+            this.lbResultado.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Confirma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNo2
+            // 
+            this.txtNo2.Location = new System.Drawing.Point(27, 117);
+            this.txtNo2.Name = "txtNo2";
+            this.txtNo2.Size = new System.Drawing.Size(100, 20);
+            this.txtNo2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Segundo Numero";
+            // 
+            // txtNo1
+            // 
+            this.txtNo1.Location = new System.Drawing.Point(27, 56);
+            this.txtNo1.Name = "txtNo1";
+            this.txtNo1.Size = new System.Drawing.Size(100, 20);
+            this.txtNo1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primeiro Numero";
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 450);
+            this.Controls.Add(this.groupBox1);
+            this.MinimizeBox = false;
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNo2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNo1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cb1;
+    }
+}
+
---
+++ primeiro projeto/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primeiro_projeto
+{
+    public partial class Calculadora : Form
+    {
+
+     
+
+
+        public Calculadora()
+        {
+            InitializeComponent();
+              
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cb1.SelectedIndex == 0)
+            {
+               lbResultado.Text = (float.Parse(txtNo1.Text) + float.Parse(txtNo2.Text)).ToString();
+
+            }
+            if (cb1.SelectedIndex == 1)
+            {
+                lbResultado.Text = (float.Parse(txtNo1.Text) - float.Parse(txtNo2.Text)).ToString();
+
+            }
+            if (cb1.SelectedIndex == 2)
+            {
+                lbResultado.Text = (float.Parse(txtNo1.Text) * float.Parse(txtNo2.Text)).ToString();
+
+            }
+            if (cb1.SelectedIndex == 3)
+            {
+                lbResultado.Text = (float.Parse(txtNo1.Text) / float.Parse(txtNo2.Text)).ToString();
+
+            }
+            txtNo1.Clear();
+            txtNo2.Clear();
+            
+            
+        }
+
+        
+    }
+}
